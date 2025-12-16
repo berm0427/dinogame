4498995140db0bd93e61098fbd3a11721e1eeec2
berm0427 <berm0427@naver.com>
2025-12-16 14:52:20 +0900

    Update dinogame.js

--- dinogame.js
+++ dinogame.js
@@ -1,14 +1,11 @@
-// 1. [핵심 수정] runnerInstance 변수를 먼저 정의해야 오류가 안 납니다.
-var runnerInstance = runnerInstance;
-
-// 2. [무적 모드] 장애물에 부딪혀도 게임오버 함수가 작동하지 않게 만듭니다.
+// 1. [무적 모드] 장애물에 부딪혀도 게임오버 함수가 작동하지 않게 만듭니다.
 runnerInstance.gameOver = function() {};
 
-// 3. [속도 조절] 원하시는 대로 속도를 설정합니다. (괄호를 사용하는 것이 정석입니다)
+// 2. [속도 조절] 원하시는 대로 속도를 설정합니다. (괄호를 사용하는 것이 정석입니다)
 // 봇이 반응할 수 있도록 적당히 빠른 100 정도로 설정했습니다. (원하면 숫자 변경 가능)
 runnerInstance.setSpeed(100); 
 
-// 4. [자동 점프 봇 로직]
+// 3. [자동 점프 봇 로직]
 var botInterval = setInterval(function() {
     
     // 게임이 멈춰있거나 끝났다면 재시작
@@ -35,3 +32,4 @@ var botInterval = setInterval(function() {
 
 // 참고: 중력(gravity)을 0으로 하면 점프 후 내려오지 않아 봇이 고장나므로,
 // 봇 모드에서는 중력을 건드리지 않는 것이 좋습니다.
+
